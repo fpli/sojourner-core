80879a48d2bd55f8b1379c3f8fbd20d8c39d194d
yxiao6 <yxiao6@ebay.com>
2021-04-07 19:40:17 +0800

    Bugfix/fix bot gap (#166)
    * remove event level bot detector and fix event level bot flag gap
    
    * remove event level bot detector and fix event level bot flag gap
    
    * fix EventMapFunction unit test

--- rt-pipeline/src/test/java/com/ebay/sojourner/rt/operator/event/EventMapFunctionTest.java
+++ rt-pipeline/src/test/java/com/ebay/sojourner/rt/operator/event/EventMapFunctionTest.java
@@ -5,13 +5,11 @@ import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.doNothing;
 import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.when;
 
-import com.ebay.sojourner.business.detector.EventBotDetector;
 import com.ebay.sojourner.business.parser.EventParser;
 import com.ebay.sojourner.common.model.RawEvent;
 import com.ebay.sojourner.common.model.UbiEvent;
-import com.google.common.collect.Sets;
+import org.apache.flink.api.common.accumulators.AverageAccumulator;
 import org.apache.flink.api.common.functions.RuntimeContext;
 import org.apache.flink.api.common.functions.util.FunctionUtils;
 import org.apache.flink.configuration.Configuration;
@@ -24,7 +22,6 @@ class EventMapFunctionTest {
   EventMapFunction mapFunction;
   RuntimeContext mockRuntimeContext = mock(RuntimeContext.class);
   EventParser mockEventParser = mock(EventParser.class);
-  EventBotDetector mockEventBotDetector = mock(EventBotDetector.class);
 
   @BeforeEach
   void setUp() {
@@ -37,20 +34,18 @@ class EventMapFunctionTest {
   void open() throws Exception {
     mapFunction.open(new Configuration());
     EventParser parser = Whitebox.getInternalState(mapFunction, "parser");
-    EventBotDetector eventBotDetector = Whitebox.getInternalState(mapFunction, "eventBotDetector");
     assertThat(parser).isNotNull();
-    assertThat(eventBotDetector).isNotNull();
   }
 
   @Test
   void map() throws Exception {
     RawEvent rawEvent = new RawEvent();
+    AverageAccumulator avgEventParserDuration = new AverageAccumulator();
     Whitebox.setInternalState(mapFunction, "parser", mockEventParser);
-    Whitebox.setInternalState(mapFunction, "eventBotDetector", mockEventBotDetector);
+    Whitebox.setInternalState(mapFunction, "avgEventParserDuration", avgEventParserDuration);
     doNothing().when(mockEventParser).parse(any(), any());
-    when(mockEventBotDetector.getBotFlagList(any())).thenReturn(Sets.newHashSet(1, 2, 3));
     UbiEvent ubiEvent = mapFunction.map(rawEvent);
-    assertThat(ubiEvent.getBotFlags().size()).isEqualTo(3);
-    assertThat(ubiEvent.getBotFlags()).contains(1,2,3);
+    assertThat(ubiEvent).isNotNull();
+    assertThat(ubiEvent.getBotFlags().size()).isEqualTo(0);
   }
 }
