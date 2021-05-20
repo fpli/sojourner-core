740d98f1371c2f14dffd2093ac30bda1bec98a3e
yxiao6 <yxiao6@ebay.com>
2021-05-20 17:16:34 +0800

    update config profile name and rotate api key/secret (#193)

--- scripts/upload_rheos_portal.sh
+++ scripts/upload_rheos_portal.sh
@@ -7,9 +7,12 @@ fi
 
 MODULE=$1
 
+API_KEY=9ce5764c30224ce59bb5eb4c95afaf02
+API_SECRET=K2h42hFcxWpNV33EgTluY7oekgrBmTl26u6HMWrTVnQgzaj6Vld4QGFUAiN8RUsz
+
 if [[ -e "${MODULE}/pom.xml" ]]; then
     echo "==================== Uploading jar to Rheos Portal ===================="
-    mvn -f ${MODULE}/pom.xml job-uploader:upload -Dusername=9c60ad5782194b0e8f91ac8607470985 -Dpassword=J51qQChoO6gQXTyM4a05phJuskjkyzXRkM5Dem4kuaCQVPmIVIzGwE1q71MJqPF5 -Dnamespace=sojourner-ubd
+    mvn -f ${MODULE}/pom.xml job-uploader:upload -Dusername=${API_KEY} -Dpassword=${API_SECRET} -Dnamespace=sojourner-ubd
 else
   echo "Cannot find module ${MODULE}"
   exit 1
