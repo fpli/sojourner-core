a7e3c45aef5c40643a668a4f8bdd20407140ce70
haxi <haxi@ebay.com>
2021-05-28 15:47:28 +0800

    Feature/enable sojsession distributor (#202)
    * build session distributor pipeline
    
    * build session distributor pipeline
    
    * build session distributor pipeline

---
+++ distributor/src/main/java/com/ebay/sojourner/distributor/pipeline/SojSessionDistJob.java
@@ -0,0 +1,60 @@
+package com.ebay.sojourner.distributor.pipeline;
+
+import static com.ebay.sojourner.common.util.Property.FLINK_APP_NAME;
+import static com.ebay.sojourner.common.util.Property.FLINK_APP_SINK_DC;
+import static com.ebay.sojourner.common.util.Property.FLINK_APP_SINK_KAFKA_TOPIC;
+import static com.ebay.sojourner.common.util.Property.FLINK_APP_SINK_OP_NAME;
+import static com.ebay.sojourner.common.util.Property.FLINK_APP_SOURCE_DC;
+import static com.ebay.sojourner.common.util.Property.FLINK_APP_SOURCE_OP_NAME;
+import static com.ebay.sojourner.common.util.Property.SINK_KAFKA_PARALLELISM;
+import static com.ebay.sojourner.common.util.Property.SOURCE_PARALLELISM;
+import static com.ebay.sojourner.flink.common.FlinkEnvUtils.getInteger;
+import static com.ebay.sojourner.flink.common.FlinkEnvUtils.getString;
+
+import com.ebay.sojourner.common.model.RawSojSessionWrapper;
+import com.ebay.sojourner.distributor.schema.RawSojSessionDeserializationSchema;
+import com.ebay.sojourner.distributor.schema.RawSojSessionWrapperSerializationSchema;
+import com.ebay.sojourner.flink.common.DataCenter;
+import com.ebay.sojourner.flink.common.FlinkEnvUtils;
+import com.ebay.sojourner.flink.connector.kafka.FlinkKafkaProducerFactory;
+import com.ebay.sojourner.flink.connector.kafka.KafkaProducerConfig;
+import com.ebay.sojourner.flink.connector.kafka.SourceDataStreamBuilder;
+import org.apache.flink.streaming.api.datastream.DataStream;
+import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+
+public class SojSessionDistJob {
+
+  public static void main(String[] args) throws Exception {
+
+    final StreamExecutionEnvironment executionEnvironment = FlinkEnvUtils.prepare(args);
+
+    final String DATA_SOURCE_OP_NAME = getString(FLINK_APP_SOURCE_OP_NAME);
+    final String DATA_SOURCE_UID = "sojsession-dist-source";
+    final String SINK_OP_NAME = getString(FLINK_APP_SINK_OP_NAME);
+    final String SINK_UID = "sojsession-dist-sink";
+
+    SourceDataStreamBuilder<RawSojSessionWrapper> dataStreamBuilder =
+        new SourceDataStreamBuilder<>(executionEnvironment);
+
+    DataStream<RawSojSessionWrapper> rawSojSessionWrapperDataStream = dataStreamBuilder
+        .dc(DataCenter.of(getString(FLINK_APP_SOURCE_DC)))
+        .parallelism(getInteger(SOURCE_PARALLELISM))
+        .operatorName(DATA_SOURCE_OP_NAME)
+        .uid(DATA_SOURCE_UID)
+        .build(new RawSojSessionDeserializationSchema());
+
+    // sink to kafka
+    KafkaProducerConfig config = KafkaProducerConfig.ofDC(getString(FLINK_APP_SINK_DC));
+    FlinkKafkaProducerFactory producerFactory = new FlinkKafkaProducerFactory(config);
+    rawSojSessionWrapperDataStream
+        .addSink(producerFactory.get(FlinkEnvUtils.getString(FLINK_APP_SINK_KAFKA_TOPIC),
+            new RawSojSessionWrapperSerializationSchema(
+                FlinkEnvUtils.getString(FLINK_APP_SINK_KAFKA_TOPIC))))
+        .setParallelism(getInteger(SINK_KAFKA_PARALLELISM))
+        .name(SINK_OP_NAME)
+        .uid(SINK_UID);
+
+    // Submit this job
+    FlinkEnvUtils.execute(executionEnvironment, getString(FLINK_APP_NAME));
+  }
+}
---
+++ distributor/src/main/java/com/ebay/sojourner/distributor/schema/RawSojSessionWrapperSerializationSchema.java
@@ -0,0 +1,26 @@
+package com.ebay.sojourner.distributor.schema;
+
+
+import com.ebay.sojourner.common.model.RawSojSessionWrapper;
+import org.apache.flink.streaming.connectors.kafka.KafkaSerializationSchema;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.jetbrains.annotations.Nullable;
+
+public class RawSojSessionWrapperSerializationSchema
+    implements KafkaSerializationSchema<RawSojSessionWrapper> {
+
+  private final String topic;
+
+  public RawSojSessionWrapperSerializationSchema(String topic) {
+    this.topic = topic;
+  }
+
+  @Override
+  public ProducerRecord<byte[], byte[]> serialize(RawSojSessionWrapper element,
+                                                  @Nullable Long timestamp) {
+
+    return new ProducerRecord<>(topic, null,
+                                element.getKey(),
+                                element.getValue());
+  }
+}
