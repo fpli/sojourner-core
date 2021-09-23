973bd9b97d7971320ab0ce1dc02187ee5b13e869
Jason Xiao <yxiao6@ebay.com>
2021-09-23 10:58:16 +0800

    update upload script

--- scripts/upload_rheos_portal.sh
+++ scripts/upload_rheos_portal.sh
@@ -15,17 +15,24 @@ API_SECRET=K2h42hFcxWpNV33EgTluY7oekgrBmTl26u6HMWrTVnQgzaj6Vld4QGFUAiN8RUsz
 
 if [[ -e "${MODULE}/pom.xml" ]]; then
   pushd ${MODULE}
+  # find artifact in target folder
   if ls target/*.jar 1> /dev/null 2>&1; then
+    # if it's in CI server env, BUILD_NUM will be set by Jenkins
     BUILD_NUM=${BUILD_NUMBER:-$(date '+%Y%m%d.%H%M%S')}
     for i in $(ls target/*.jar); do
-      mv "$i" "`echo $i | sed "s/-SNAPSHOT/.${BUILD_NUM}/"`";
+      if [[ "${i%.jar}" == *-SNAPSHOT ]]; then
+        mv "$i" "`echo $i | sed "s/-SNAPSHOT/.${BUILD_NUM}/"`";
+      elif [[ "$(cat ../pomVersion)" == *-SNAPSHOT  ]]; then
+        # build num already generated
+        BUILD_NUM=${BUILD_NUMBER:-${i:(-19):15}}
+      fi
     done
+    echo "Build number is: $BUILD_NUM"
+
     JAR_NAME=$(mvn help:evaluate -Dexpression=project.artifactId -q -DforceStdout)
     JAR_TAG=$(cat ../pomVersion | sed "s/-SNAPSHOT/.${BUILD_NUM}/")
-    # echo ${JAR_NAME}
-    # echo ${JAR_TAG}
-    echo "==================== Uploading jar to Rheos Portal ===================="
 
+    echo "==================== Uploading jar to Rheos Portal ===================="
     mvn job-uploader:upload \
       -Dusername=${API_KEY} \
       -Dpassword=${API_SECRET} \
