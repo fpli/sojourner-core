c8f18949beb2e717e36901959fcbf28dbc13b739
Jason Xiao <yxiao6@ebay.com>
2022-07-21 17:09:53 +0800

    change srp topic to new srpGist topic (#280)

--- distributor/src/main/java/com/ebay/sojourner/distributor/route/rule/SrpRouteRule.java
+++ distributor/src/main/java/com/ebay/sojourner/distributor/route/rule/SrpRouteRule.java
@@ -5,7 +5,7 @@ import static com.ebay.sojourner.common.constant.ApplicationPayloadTags.SRP_GIST
 import com.ebay.sojourner.common.model.SojEvent;
 import com.ebay.sojourner.distributor.route.Route;
 
-@Route(key = "srp")
+@Route(key = "srpGist")
 public class SrpRouteRule extends AbstractSojEventRouteRule {
 
     @Override
