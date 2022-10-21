017043dfd6ef1ac290208284cfbb40d6e716c073
cmao <cmao@ebay.com>
2022-10-21 13:31:54 +0800

    Feature/reduce job dup code (#286)
    * Reduce duplicate code among realtime jobs
    * Add dev job and profile for testing in IDE

--- rt-pipeline/src/main/java/com/ebay/sojourner/rt/pipeline/SojournerRTJob.java
+++ rt-pipeline/src/main/java/com/ebay/sojourner/rt/pipeline/SojournerRTJob.java
@@ -75,12 +75,31 @@ import org.apache.flink.types.Either;
 public class SojournerRTJob {
 
   public static void main(String[] args) throws Exception {
+    new SojournerRTJob().run(args);
+  }
+
+  public void run(String[] args) throws Exception {
 
     // 0.0 Prepare execution environment
     // 0.1 UBI configuration
     // 0.2 Flink configuration
-    final StreamExecutionEnvironment executionEnvironment = FlinkEnvUtils.prepare(args);
+    StreamExecutionEnvironment executionEnvironment = prepareStreamExecutionEnvironment(args);
 
+    DataStream<UbiEvent> ubiEventDataStream = generateEventDataStream(executionEnvironment);
+
+    doSessionizationAndBotDetection(ubiEventDataStream);
+
+    // Submit this job
+    FlinkEnvUtils.execute(executionEnvironment, getString(Property.FLINK_APP_NAME));
+  }
+
+  protected StreamExecutionEnvironment prepareStreamExecutionEnvironment(String[] args) {
+    StreamExecutionEnvironment env = FlinkEnvUtils.prepare(args);
+    return env;
+  }
+
+  protected DataStream<UbiEvent> generateEventDataStream(
+          StreamExecutionEnvironment executionEnvironment) {
     // 1. Rheos Consumer
     // 1.1 Consume RawEvent from Rheos PathFinder topic
     // 1.2 Assign timestamps and emit watermarks.
@@ -168,7 +187,10 @@ public class SojournerRTJob {
     DataStream<UbiEvent> ubiEventDataStream = ubiEventDataStreamForLVS
         .union(ubiEventDataStreamForSLC)
         .union(ubiEventDataStreamForRNO);
+    return ubiEventDataStream;
+  }
 
+  protected void doSessionizationAndBotDetection(DataStream<UbiEvent> ubiEventDataStream) {
     // refine windowsoperator
     // 3. Session Operator
     // 3.1 Session window
@@ -490,8 +512,5 @@ public class SojournerRTJob {
         .slotSharingGroup(getString(Property.SESSION_SLOT_SHARE_GROUP))
         .name("Late SojEvent")
         .uid("late-sojevent-sink");
-
-    // Submit this job
-    FlinkEnvUtils.execute(executionEnvironment, getString(Property.FLINK_APP_NAME));
   }
 }
---
+++ rt-pipeline/src/main/java/com/ebay/sojourner/rt/pipeline/SojournerRTJobForDev.java
@@ -0,0 +1,17 @@
+package com.ebay.sojourner.rt.pipeline;
+
+import com.ebay.sojourner.flink.common.FlinkEnvUtils;
+import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+
+public class SojournerRTJobForDev extends SojournerRTJobForQA {
+
+  public static void main(String[] args) throws Exception {
+    new SojournerRTJobForDev().run(args);
+  }
+
+  protected StreamExecutionEnvironment prepareStreamExecutionEnvironment(String[] args) {
+    StreamExecutionEnvironment env = FlinkEnvUtils.prepareLocal(args);
+//    env.disableOperatorChaining();
+    return env;
+  }
+}
