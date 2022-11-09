9e6f0a863f3d93560775e491a8cac5c1c898e055
Jason Xiao <yxiao6@ebay.com>
2022-11-09 17:40:49 +0800

    update scandal trk filtering logic (#289)

--- distributor/src/main/java/com/ebay/sojourner/distributor/route/rule/ScandalTrkRouteRule.java
+++ distributor/src/main/java/com/ebay/sojourner/distributor/route/rule/ScandalTrkRouteRule.java
@@ -6,9 +6,12 @@ import com.ebay.sojourner.distributor.route.Route;
 @Route(key = "scandal-trk")
 public class ScandalTrkRouteRule extends AbstractSojEventRouteRule {
 
+  private static final Integer PAGE_ID = 3792362;
+
   @Override
   public boolean match(SojEvent sojEvent) {
-    return sojEvent.getApplicationPayload() != null
-        && sojEvent.getApplicationPayload().containsKey("scandal_trk");
+    return PAGE_ID.equals(sojEvent.getPageId())
+        || (sojEvent.getApplicationPayload() != null
+            && sojEvent.getApplicationPayload().containsKey("scandal_trk"));
   }
 }
